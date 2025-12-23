4de17d18e062a3a53bed88d1cb3cfce5f6af1ff7
CristianDavidChinoBejar <cristianrg095@gmail.com>
2025-12-23 16:28:07 -0300

    ADD: Se agrega context a los test del carrito de compras

--- cypress/e2e/pages/cartPage.js
+++ cypress/e2e/pages/cartPage.js
@@ -1,13 +1,20 @@
 class CartPage {
     elements = {
         addToCartSauceLabsBackpackButton: () => cy.get('[data-test="add-to-cart-sauce-labs-backpack"]'),
+        addToCartSauceLabsBikeLight: () => cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]'),
         cartIcon: () => cy.get('[data-test="shopping-cart-badge"]'),
         inventoryItem: () => cy.get('[data-test="inventory-item"]'),
-        removeItemButton: () => cy.get('[data-test="remove-sauce-labs-backpack"]')
+        removeItemButton: () => cy.get('[data-test="remove-sauce-labs-backpack"]'),
+        continueShoppingButton: () => cy.get('[data-test="continue-shopping"]'),
+        checkoutButton: () => cy.get('[data-test="checkout"]')
     }
 
     clickAddToCartSauceLabsBackpack() {
         this.elements.addToCartSauceLabsBackpackButton().click();
+    }
+
+    clickAddToCartSauceLabsBikeLight() {
+        this.elements.addToCartSauceLabsBikeLight().click();
     }
 
     clickCartIcon() {
@@ -16,6 +23,13 @@ class CartPage {
 
     clickRemoveItemButton() {
         this.elements.removeItemButton().click();
+    }
+
+    clickContinueShoppingButton() {
+        this.elements.continueShoppingButton().click();
+    }
+    clickCheckoutButton() {
+        this.elements.checkoutButton().click();
     }
 }
 
--- cypress/e2e/tests/cart.cy.js
+++ cypress/e2e/tests/cart.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import CartPage from '../pages/cartPage'
+import cartPage from '../pages/cartPage'
 
 describe('Validación del carrito de compras', () => {
   beforeEach(() => {
@@ -10,22 +10,40 @@ describe('Validación del carrito de compras', () => {
       cy.login(standardUser.username, standardUser.password);
     })
 
-    CartPage.clickAddToCartSauceLabsBackpack()
-    CartPage.elements.cartIcon().should('have.text', '1')
-    CartPage.clickCartIcon()
+    cartPage.clickAddToCartSauceLabsBackpack()
+    cartPage.elements.cartIcon().should('have.text', '1')
+    cartPage.clickCartIcon()
   })
 
-  it('Validar que un producto se agregue correctamente', () => {
-    CartPage.elements.inventoryItem().should('have.length', 1)
-  });
+  context('Agregar productos al carrito', () => {
+    it('Validar que un producto se agregue correctamente', () => {
+      cartPage.elements.inventoryItem().should('have.length', 1)
+    });
 
-  it('Verificar que se elimina el único producto en el carrito', () => {
-    CartPage.clickRemoveItemButton()
-    CartPage.elements.inventoryItem().should('have.length', 0)
-  });
+    it('Verificar que se pueda continuar comprando desde el carrito de compras', () => {
+      cartPage.clickContinueShoppingButton()
+      cy.url().should('include', 'inventory.html')
+      cartPage.clickAddToCartSauceLabsBikeLight()
+      cartPage.elements.cartIcon().should('have.text', '2')
+    });
+  })
 
-  it('Verificar que los productos permanezcan en el carrito luego de hacer un refresh a la página', () => {
-    cy.reload()
-    CartPage.elements.inventoryItem().should('have.length', 1)
-  });
+  context('Eliminar productos del carrito', () => {
+    it('Verificar que se elimina el único producto en el carrito', () => {
+      cartPage.clickRemoveItemButton()
+      cartPage.elements.inventoryItem().should('have.length', 0)
+    });
+  })
+
+  context('Navegación', () => {
+    it('Verificar que los productos permanezcan en el carrito luego de hacer un refresh a la página', () => {
+      cy.reload()
+      cartPage.elements.inventoryItem().should('have.length', 1)
+    });
+
+    it('Verificar redirección al Checkout inicial', () => {
+      cartPage.clickCheckoutButton()
+      cy.url().should('include', 'checkout-step-one.html')
+    });
+  })
 }) 
