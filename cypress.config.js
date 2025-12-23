96eaaa30cbf4a833cff800a829bf1916139e50f2
CristianDavidChinoBejar <cristianrg095@gmail.com>
2025-12-23 13:20:35 -0300

    ADD: Se agrega Environment en Allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require('cypress')
 const allureWriter = require('@shelex/cypress-allure-plugin/writer')
+const { writeAllureEnvironment } = require('./cypress/support/environmentHelper');
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config)
+      const browserName = config.browser?.name || 'Chrome';
+      writeAllureEnvironment(browserName);
       return config
     },
     baseUrl: 'https://www.saucedemo.com',
